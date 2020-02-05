b2d0901a248427e444b0bbc8518dee0a7dcf19bb
RomaVintonyak <19roman23@gmail.com>
2020-02-05 16:00:31 +0200

    create how we work section

--- js/block/feature.js
+++ js/block/feature.js
@@ -1,11 +1,12 @@
 jQuery(document).ready(function(){
     "use script"
     var elemCount = $(".feature__count");
+    var setTime = 12000;
     elemCount.each(function () {
         $(this).prop('Counter', 0).animate({
             Counter: $(this).text()
         }, {
-            duration: 6000,
+            duration: setTime,
             easing: 'swing',
             step: function (now) {
                 $(this).text(Math.ceil(now));
