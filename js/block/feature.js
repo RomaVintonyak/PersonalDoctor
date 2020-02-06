aab4ecbc5e71a0b24c15cd7bf6ba051705358da6
RomaVintonyak <19roman23@gmail.com>
2020-02-06 13:28:06 +0200

    create offset block animation

--- js/block/feature.js
+++ js/block/feature.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function(){
     "use script"
     var elemCount = $(".feature__count");
-    var setTime = 12000;
+    var setTime = 20000;
     elemCount.each(function () {
         $(this).prop('Counter', 0).animate({
             Counter: $(this).text()
