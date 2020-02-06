efb12994501d6f1b0985bae8d90e5b69bc5cb6b2
RomaVintonyak <19roman23@gmail.com>
2020-02-06 14:04:59 +0200

    finished acardeon

---
+++ js/block/acardeon.js
@@ -0,0 +1,22 @@
+jQuery(document).ready(function(){
+  "use script"
+  var acardeon = function () {
+    var data = $(".right__acardeon").attr('data-acardeon');
+    $(".acardeon__header").on("click", function () {
+        if (data === "close") {
+            $(".acardeon__body").slideUp();
+            if ($(this).hasClass("acardeon__header--active")) {
+                $(this).toggleClass("acardeon__header--active");
+            } else {
+                $(".acardeon__header").removeClass("acardeon__header--active");
+                $(this).toggleClass("acardeon__header--active");
+            }
+        }
+        else {
+            $(this).toggleClass("acardeon__header--active");
+        }
+        $(this).next(".acardeon__body").not(":animated").slideToggle();
+    });
+}
+acardeon();
+});
