dbc4346d7bdffffdecb0ec1490f3e914010774fe
roman <19roman23@gmail.com>
2020-02-10 12:07:05 +0200

    modified script fixed header

--- js/block/fixed__header.js
+++ js/block/fixed__header.js
@@ -1,11 +1,21 @@
-jQuery(document).ready(function(){
+jQuery(document).ready(function () {
     "use script"
-    $(window).on("scroll", function(){
-        if($(this).scrollTop() >= 150){
-            $("header").addClass("fixed");
-        }
-        else{
-            $("header").removeClass("fixed");
-        }
+    var introH = $("#intro").innerHeight();
+    var scrollOfset = $(window).scrollTop();
+    var header = $("header");
+    checkScroll(scrollOfset);
+    $(window).on("scroll", function () {
+        scrollOfset = $(this).scrollTop();
+        checkScroll(scrollOfset);
     });
+    function checkScroll(scrollOfset) {
+        scrollOfset = $(this).scrollTop();
+        if (scrollOfset >= introH) {
+            header.addClass("fixed__header");
+            $(".header__top").css("margin-top", 0);
+        } else {
+            header.removeClass("fixed__header");
+            $(".header__top").css("margin-top", "1rem");
+        }
+    }
 });
