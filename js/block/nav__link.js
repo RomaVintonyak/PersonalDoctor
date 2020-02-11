ec5cfa1e47942660ed6c0fd0b31564301e1919be
RomaVintonyak <19roman23@gmail.com>
2020-02-11 15:36:41 +0200

    create page about

--- js/block/nav__link.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function(){
-    "use script"
-    var navLink = $(".nav__link");
-    navLink.on("click", function(){
-        $(this).toggleClass("nav__link--active");
-    });
-});
